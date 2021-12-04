833c01dff975f1e57e4bfec573963c651a38ba88
Florian Schweitzer <florian.schweitzer@gmail.com>
2021-12-04 23:44:47 +0100

    Some charts

--- utility/helpers.py
+++ utility/helpers.py
@@ -6,7 +6,7 @@ def addDateTypeColumn(df, colName='XDate'):
     dfc[colName] = dfc.apply(lambda x: dt.strptime(x['Date'],"%Y-%m-%d"), axis=1)
     return dfc
 
-def addDayOffStreaks(df):
+def addDayOffStreaks(df, legLabel='Off days'):
     import pandas as pd
     import numpy as np
 
@@ -22,7 +22,38 @@ def addDayOffStreaks(df):
     dfc = dfc.groupby('streak_id').max().reset_index()
     dfc = dfc.loc[dfc['Day_Off']]
 
+    labeled = False
+
     for idx, streak in dfc.iterrows():
         end = streak['XDate']
         start = end - timedelta(days = streak['streak_counter'])
-        plt.axvspan(start,end, facecolor='grey', alpha=0.5)
+        if labeled:
+            plt.axvspan(start,end, facecolor='grey', alpha=0.5)
+        else:
+            plt.axvspan(start,end, facecolor='grey', alpha=0.5, label=legLabel)
+            labeled = True
+
+def addProvince(df, provinceId):
+    import pandas as pd
+    import numpy as np
+
+    import matplotlib.pyplot as plt
+    import utility.init as util
+
+    fig, ax1 = plt.subplots()
+
+    x = df.loc[df['Province_Id']== provinceId, 'XDate']
+    y = df.loc[df['Province_Id']== provinceId, 'N_Day_Rate_Change_Sliding_Window']
+    ax1.set_xlabel( util.timeframe_start + " - " + util.timeframe_end ,fontdict=util.font)
+    ax1.set_ylabel( str(util.incident_window_size)+"-day infection rate change (sliding window)" ,fontdict=util.font)
+    ax1.plot(x,y, label = util.class_labels[provinceId], color = util.class_colors[provinceId])
+
+def sketchGraph(df, provinces, ref_df, legLabel='Off days'):
+    import matplotlib.pyplot as plt
+
+    for prov in provinces:
+        addProvince(df, prov)
+    
+    addDayOffStreaks(ref_df, legLabel)
+    plt.legend()
+    plt.show()
--- utility/init.py
+++ utility/init.py
@@ -25,7 +25,8 @@ province_id = {
     'StädteRegion Aachen':31,
     'LK Düren':32,
     'LK Heinsberg':33,
-    'LK Euskirchen':34
+    'LK Euskirchen':34,
+    'Euregio Maas-Rhine': 40
 }
 
 # inhabitants
@@ -38,27 +39,49 @@ province_size = {
     31: 556631,
     32: 265140,
     33: 256458,
-    34: 194359
+    34: 194359,
+    40: 0
 }
 
 for key, val in province_size.items():
     if key < 20:
         province_size[10] += val
-    if key > 30:
+    if key > 30 and key < 40:
         province_size[30] += val
+
+province_size[40] = province_size[10]+province_size[20]+province_size[30]
 
 class_colors = {
     10: 'xkcd:dark blue',
     11: 'xkcd:blue',
     12: 'xkcd:light blue',
-    20:'xkcd:red',
-    30: 'xkcd:sea green' ,
-    31:'xkcd:aqua',
-    32:'xkcd:teal',
-    33:'xkcd:green',
-    34:'xkcd:dark green' 
-    }
+    20: 'xkcd:red',
+    30: 'xkcd:dark green' ,
+    31: 'xkcd:aqua',
+    32: 'xkcd:teal',
+    33: 'xkcd:green',
+    34: 'xkcd:sea green',
+    40: 'xkcd:eggplant'
+}
 
+class_labels = {
+    10: 'Belgium',
+    11: 'Liege, BE',
+    12: 'Limburg, BE',
+    20: 'Netherlands',
+    30: 'Germany',
+    31: 'StädteRegion Aachen, DE',
+    32: 'LK Düren, DE',
+    33: 'LK Heinsberg, DE',
+    34: 'LK Euskirchen, DE',
+    40: 'Euregio Maas-Rhine'
+}
+
+font = {#'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'fontsize': 'small'
+        }
 # emr_cols = ['Date', 'Province_Id', 'Daily_Total', 'Daily_100k', 'N_Day_Rate','N_Day_Rate_Change','N_Day_Rate_Change_Sliding_Window']
 
 # ref_cols = ['Date', 'Province_Id', 'Work_Commute_Allowed', 'Leisure_Travel_Allowed', 'Holiday', 'Vacation']
